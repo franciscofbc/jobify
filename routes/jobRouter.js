7a0585c62ad0f64151bf91c4c8f17ea70a5c8825
Francisco Berti da Cruz <fbc991@live.com>
2023-12-28 11:22:12 -0300

    validateJobInput done

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,17 +1,23 @@
-import { Router } from "express";
+import { Router } from 'express';
+const router = Router();
+
 import {
   createJob,
   deleteJob,
   updateJob,
   getAllJobs,
   getSingleJob,
-} from "../controllers/jobController.js";
-const router = Router();
+} from '../controllers/jobController.js';
+import { validateJobInput } from '../middleware/validationMiddleware.js';
 
 // router.get('/', getAllJobs);
 // router.post('/', createJob);
 
-router.route("/").get(getAllJobs).post(createJob);
-router.route("/:id").get(getSingleJob).patch(updateJob).delete(deleteJob);
+router.route('/').get(getAllJobs).post(validateJobInput, createJob);
+router
+  .route('/:id')
+  .get(getSingleJob)
+  .patch(validateJobInput, updateJob)
+  .delete(deleteJob);
 
 export default router;
