f9c7b4e46a9e3c10491136441b58af2185364d25
Francisco Berti da Cruz <fbc991@live.com>
2024-01-05 15:24:59 -0300

    Hash Password Done

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/UserModel.js';
+import { hashPassword } from '../utils/passwordUtils.js';
+
+export const register = async (req, res) => {
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
+
+  req.body.password = await hashPassword(req.body.password);
+
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' });
+};
+
+export const login = async (req, res) => {
+  res.send('login');
+};
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import mongoose from 'mongoose';
 
 //routes
 import jobRouter from './routes/jobRouter.js';
+import authRouter from './routes/authRouter.js';
 
 //middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
@@ -28,6 +29,7 @@ app.post('/', (req, res) => {
 });
 
 app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/auth', authRouter);
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
