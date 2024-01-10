c627cd8609bd9e51e698f92ad99326086f3e66c2
Francisco Berti da Cruz <fbc991@live.com>
2024-01-09 21:52:10 -0300

    register user complete

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-
-fetch('/api/v1/test')
-  .then((res) => res.json())
-  .then((data) => console.log(data));
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,46 +1,47 @@
-import { Link } from "react-router-dom"
-import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
-import { FormRow, Logo } from '../components'
+import { Form, Link, redirect } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
+import customFetch from "../utils/customFetch";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post("/auth/register", data);
+    return redirect("/login");
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
 const Register = () => {
-    return (
-        <Wrapper>
-            <form className="form">
-                <Logo />
-                <h4>register</h4>
-                <FormRow
-                    type='text'
-                    name='name'
-                    defaultValue='francisco' />
-                <FormRow
-                    type='text'
-                    name='lastName'
-                    labelText='last name'
-                    defaultValue='da cruz' />
-                <FormRow
-                    type='text'
-                    name='location'
-                    defaultValue='ijui' />
-                <FormRow
-                    type='email'
-                    name='email'
-                    defaultValue='fbc@live.com' />
-                <FormRow
-                    type='password'
-                    name='password'
-                    defaultValue='secret' />
-                <button type="submit"
-                    className="btn btn-block">
-                    submit
-                </button>
-                <p>
-                    already a member?
-                    <Link to='/login' className="member-btn">
-                        login
-                    </Link>
-                </p>
-            </form >
-        </Wrapper >
-    )
-}
-export default Register
+  return (
+    <Wrapper>
+      <Form method="POST" className="form">
+        <Logo />
+        <h4>register</h4>
+        <FormRow type="text" name="name" defaultValue="francisco" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="da cruz"
+        />
+        <FormRow type="text" name="location" defaultValue="ijui" />
+        <FormRow type="email" name="email" defaultValue="fbc@live.com" />
+        <FormRow type="password" name="password" defaultValue="secret123" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          already a member?
+          <Link to="/login" className="member-btn">
+            login
+          </Link>
+        </p>
+      </Form>
+    </Wrapper>
+  );
+};
+export default Register;
