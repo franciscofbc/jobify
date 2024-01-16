735ae4c105ac6d708c8bcac4456539db39d3ee18
Francisco Berti da Cruz <fbc991@live.com>
2024-01-15 22:08:34 -0300

    update user functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
-import { StatusCodes } from 'http-status-codes';
-import Job from '../models/JobModel.js';
-import User from '../models/UserModel.js';
+import { StatusCodes } from "http-status-codes";
+import Job from "../models/JobModel.js";
+import User from "../models/UserModel.js";
+import cloudinary from "cloudinary";
+import { promises as fs } from "fs";
 
 export const getCurretUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -16,8 +18,21 @@ export const getApplicationStats = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
-  const obj = { ...req.body };
-  delete obj.password;
-  const updateUser = await User.findByIdAndUpdate(req.user.userId, obj);
-  res.status(StatusCodes.OK).json({ msg: 'update user' });
+  const newUser = { ...req.body };
+  delete newUser.password;
+
+  if (req.file) {
+    const response = await cloudinary.v2.uploader.upload(req.file.path);
+    await fs.unlink(req.file.path);
+    newUser.avatar = response.secure_url;
+    newUser.avatarPublicId = response.public_id;
+  }
+
+  const oldUser = await User.findByIdAndUpdate(req.user.userId, newUser);
+
+  if (req.file && oldUser.avatarPublicId) {
+    await cloudinary.v2.uploader.destroy(oldUser.avatarPublicId);
+  }
+
+  res.status(StatusCodes.OK).json({ msg: "user updated" });
 };
