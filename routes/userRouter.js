f4687eb04c94a5a920e53139b1ba1bb58a54c544
Francisco Berti da Cruz <fbc991@live.com>
2024-01-15 21:32:54 -0300

    cloudinary

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,19 +1,25 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
 import {
   getCurretUser,
   getApplicationStats,
   updateUser,
-} from '../controllers/userController.js';
-import { validateUpdateUserInput } from '../middleware/validationMiddleware.js';
-import { authorizePermissions } from '../middleware/authMiddleware.js';
+} from "../controllers/userController.js";
+import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
+import { authorizePermissions } from "../middleware/authMiddleware.js";
+import upload from "../middleware/multerMiddleware.js";
 
-router.get('/current-user', getCurretUser);
+router.get("/current-user", getCurretUser);
 router.get(
-  '/admin/app-stats',
-  authorizePermissions('admin'),
+  "/admin/app-stats",
+  authorizePermissions("admin"),
   getApplicationStats
 );
-router.patch('/update-user', validateUpdateUserInput, updateUser);
+router.patch(
+  "/update-user",
+  upload.single("avatar"),
+  validateUpdateUserInput,
+  updateUser
+);
 
 export default router;
