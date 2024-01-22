51d84c1c5aa2bdfaa3f3fcdfe57f240d7e74efb6
Francisco Berti da Cruz <fbc991@live.com>
2024-01-22 20:29:45 -0300

    fix multer middleware

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -5,7 +5,7 @@ import path from "path";
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-const parser = DataParser();
+const parser = new DataParser();
 export const formatImage = (file) => {
   const fileExtension = path.extname(file.originalname).toString();
   return parser.format(fileExtension, file.buffer).content;
