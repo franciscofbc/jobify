8516e5b17b8cd0007526aa7e610ef4587d6a7a58
Francisco Berti da Cruz <fbc991@live.com>
2024-01-22 15:45:30 -0300

    fix front-end

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ cloudinary.config({
 //public
 const __dirname = dirname(fileURLToPath(import.meta.url));
 console.log(__dirname);
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -57,7 +57,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use('*', (req, res) => {
